49b2f52815aa80df26611cbb624c64ca958abcd8
Clint James <clint@inspireteam.com>
2019-10-28 15:29:37 +0800

    updates in virtual scroll

--- src/VirtualScroll.ts
+++ src/VirtualScroll.ts
@@ -1,10 +1,36 @@
 import { ipcRenderer } from "electron";
 
 const DUMMY = [
-    { id: 1, text: "hello" },
-    { id: 2, text: "there" },
-    { id: 3, text: "general" },
-    { id: 4, text: "kinobi" },
+    { x: 1, y: 3 },
+    { x: 2, y: 3 },
+    { x: 3, y: 3 },
+    { x: 4, y: 3 },
+    { x: 5, y: 3 },
+    { x: 6, y: 3 },
+    { x: 7, y: 3 },
+    { x: 8, y: 3 },
+    { x: 9, y: 3 },
+    { x: 10, y: 3 },
+    { x: 11, y: 3 },
+    { x: 12, y: 3 },
+    { x: 13, y: 3 },
+    { x: 14, y: 3 },
+    { x: 15, y: 3 },
+    { x: 16, y: 3 },
+    { x: 17, y: 3 },
+    { x: 18, y: 3 },
+    { x: 19, y: 3 },
+    { x: 20, y: 3 },
+    { x: 21, y: 3 },
+    { x: 22, y: 3 },
+    { x: 23, y: 3 },
+    { x: 24, y: 3 },
+    { x: 25, y: 3 },
+    { x: 26, y: 3 },
+    { x: 27, y: 3 },
+    { x: 28, y: 3 },
+    { x: 29, y: 3 },
+    { x: 30, y: 3 },
 ];
 
 class VirtualList {
@@ -12,32 +38,48 @@ class VirtualList {
     private totalRows: number;
     private cachedItemsLen: number;
     private itemHeight: number;
-    private items: object[];
+    private items: IList[];
     private screenItemsLen: number;
     private lastScrolled: number;
     private maxBuffer: number;
     private lastRepaintY: number;
-    private rmNodeInterval: NodeJS.Timeout;
+    scroller: HTMLDivElement;
+    width: any;
+    height: any;
+    first: number;
 
     constructor(config: IConfig) {
-        const width = (config && config.w + "px") || "100%";
-        const height = (config && config.h + "px") || "100%";
-        const itemHeight = config.itemHeight;
-        const scroller = this.createScroller(itemHeight * this.totalRows);
+        this.width = (config && config.w + "px") || "100%";
+        this.height = (config && config.h + "px") || "100%";
+        this.scroller = this.createScroller(this.itemHeight * this.totalRows);
 
+        this.itemHeight = config.itemHeight;
         this.onScroll = this.onScroll.bind(this);
         this.items = DUMMY;
-        this.totalRows = config.totalRows; // TODO remove
-        this.container = this.createContainer(width, height);
-        this.container.appendChild(scroller);
-        this.screenItemsLen = Math.ceil(config.h / itemHeight);
+        this.totalRows = this.items.length; // TODO remove
+        this.container = this.createContainer(this.width, this.height);
+        this.container.appendChild(this.scroller);
+        this.screenItemsLen = Math.ceil(config.h / this.itemHeight);
         this.cachedItemsLen = this.screenItemsLen * 3;
-        this.renderChunk(this.container, 0);
-        // let lastRepaintY;
-        this.maxBuffer = this.screenItemsLen * itemHeight;
+        this.maxBuffer = this.screenItemsLen * this.itemHeight;
         this.lastScrolled = 0;
+        this.lastRepaintY = 0;
+        this.renderChunk(this.container, 0);
+        this.awaitLogs();
+        this.removeBadNodes();
+        this.first = 0;
 
-        this.rmNodeInterval = setInterval(function() {
+        this.container.addEventListener("scroll", this.onScroll);
+    }
+
+    private awaitLogs() {
+        ipcRenderer.on("cursor-update", (event, log) => {
+            this.items.push(log)
+        });
+    }
+
+    private removeBadNodes() {
+        setInterval(function () {
             if (Date.now() - this.lastScrolled > 100) {
                 const badNodes = document.querySelectorAll('[data-rm="1"]');
                 for (let i = 0, l = badNodes.length; i < l; i++) {
@@ -45,16 +87,13 @@ class VirtualList {
                 }
             }
         }, 300);
-
-        // ipcRenderer.on("cursor-update", (event, log) => {
-        //     this.items.push(log)
-        // });
-        // this.container.addEventListener("scroll", this.onScroll);
     }
 
     private createRow(i: number) {
-        let item: HTMLDivElement;
-        const itemText = document.createTextNode((!!this.items[i] && this.items[i].text) || "no text");
+        let item: HTMLElement;
+        const index = Math.floor(i);
+        const text = (!!this.items[index] && `${this.items[index].x}, ${this.items[index].y}`) || "no text";
+        const itemText = document.createTextNode(text);
         item = document.createElement("div");
         item.style.height = this.itemHeight + "px";
         item.appendChild(itemText);
@@ -104,28 +143,40 @@ class VirtualList {
     private onScroll(e: Event) {
         const scrollTop = e.target.scrollTop;
         if (!this.lastRepaintY || Math.abs(scrollTop - this.lastRepaintY) > this.maxBuffer) {
-            const first = (scrollTop / this.itemHeight) - this.screenItemsLen;
-            this.renderChunk(this.container, first < 0 ? 0 : first);
+            this.first = (scrollTop / this.itemHeight) - this.screenItemsLen;
+            this.renderChunk(this.container, this.first < 0 ? 0 : this.first);
             this.lastRepaintY = scrollTop;
         }
 
+        // console.log(parseInt(this.height) - scrollTop)
+        console.log(e.target)
+        // console.log(scrollTop)
+        // if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        //     console.log("you're at the bottom of the page")
+        // }
         this.lastScrolled = Date.now();
+
         e.preventDefault();
     }
+
+
 }
 
 const list = new VirtualList({
     h: 300,
     itemHeight: 24,
-    totalRows: 1000,
     w: 300,
 });
 
-interface IConfig {
+type IConfig = {
     h: number;
     itemHeight: number;
-    totalRows: number;
     w: number;
+}
+
+type IList = {
+    x: number;
+    y: number;
 }
 
 document.getElementById("container").appendChild(list.container);
