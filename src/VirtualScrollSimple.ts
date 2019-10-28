7e705ca0a921f30ca923b23ebd512062587d045c
Clint James <clint@inspireteam.com>
2019-10-28 18:08:52 +0800

    working copy

--- src/VirtualScrollSimple.ts
+++ src/VirtualScrollSimple.ts
@@ -1,37 +1,5 @@
 import { ipcRenderer } from "electron";
-
-const DUMMY = [
-    { x: 1, y: 3 },
-    { x: 2, y: 3 },
-    { x: 3, y: 3 },
-    { x: 4, y: 3 },
-    { x: 5, y: 3 },
-    { x: 6, y: 3 },
-    { x: 7, y: 3 },
-    { x: 8, y: 3 },
-    { x: 9, y: 3 },
-    { x: 10, y: 3 },
-    { x: 11, y: 3 },
-    { x: 12, y: 3 },
-    { x: 13, y: 3 },
-    { x: 14, y: 3 },
-    { x: 15, y: 3 },
-    { x: 16, y: 3 },
-    { x: 17, y: 3 },
-    { x: 18, y: 3 },
-    { x: 19, y: 3 },
-    { x: 20, y: 3 },
-    { x: 21, y: 3 },
-    { x: 22, y: 3 },
-    { x: 23, y: 3 },
-    { x: 24, y: 3 },
-    { x: 25, y: 3 },
-    { x: 26, y: 3 },
-    { x: 27, y: 3 },
-    { x: 28, y: 3 },
-    { x: 29, y: 3 },
-    { x: 30, y: 3 },
-];
+import DUMMY from "./dummy"
 
 class VirtualList {
     public container: Element;
